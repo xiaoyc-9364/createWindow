3018e9a7f5c3d08f38ccc43e744fbe72da94f40d
xiaoyc <739759506@qq.com>
2017-12-27 09:36:01 +0800

    修改事件的绑定方式

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     class PopWindow {
-        
         constructor($ele, opts) {
             const defaults =  {
                 title: 'window',
@@ -14,7 +13,10 @@ $(document).ready(function() {
 
         init() {
             this.createNode();
-            this.addHandler();
+            this.destory();
+            this.move();
+            this.minWindow();
+            // this.addHandler();
             this.getCurrentWindow();
             this.changeSize();
         }
@@ -31,93 +33,91 @@ $(document).ready(function() {
                             <button class="pop-window-close"></button>
                         </div>
                     </div>
-                    <div class="pop-window-content">${options.content}</div>
+                    <div class="pop-window-content"><p>${options.content}</p></div>
                 </div>`).css({
                         left: options.left + 'px',
                         top: options.top + 'px',
                         width: options.width + 'px',
+                        // height:options.height + 'px'
                         })
                         .appendTo(this.$container);
-            
-            this.$popWindow.find('.pop-window-content').height(options.height - $('.pop-window-header').height());
-
-
+            const headerHeight = this.$popWindow.find('.pop-window-header').height();
+            this.$popWindow.find('.pop-window-content').height(options.height - headerHeight);
+                       
         }
 
-        addHandler() {
+        minWindow() {
             const _this = this;
-            $('.pop-window-close').each(function() { //关闭窗口
-                $(this).mouseover(function(e) {
-                    e.stopPropagation();
+            this.$popWindow.find('.contents-min').click(function() {
+                _this.$popWindow.find('.pop-window-content').slideToggle();
+            });
+        }
 
-                });
-                $(this).click(function(e) {
-                    e.stopPropagation();
-                    // e.preventDefluat();
-                    $(this).parents('.pop-window').remove();
-                });
-            });
+        maxWindow() {
+            const _this = this;
+
+
+
             
-            $('.contents-min').each(function() { //切换内容显示
-                $(this).mouseover(function(e) {
-                    e.stopPropagation();
-                });
-                $(this).click(function(e) {
-                    console.log(e.target);
-                    e.stopPropagation();
-                    // e.preventDefluat();
-                    $(this).parents('.pop-window').find('.pop-window-content').slideToggle();
-                });
-            });
+        }
+
+        move() {
+            const _this = this;
             const handler = function(e) {
                 const $self = $(this);
-
+                //控制窗口的按钮不可用来移动窗口
                 if (e.target.nodeName.toLowerCase() === 'button') {
                     return false;
                 } 
 
+                //变换手型
                 $self.css('cursor', 'move');
 
-                const diffX = e.pageX,
-                      diffY = e.pageY,
-                      winowLeft = _this.$popWindow.position().left,
-                      windowTop = _this.$popWindow.position().top,
-                      windowWidth = _this.$popWindow.outerWidth(),
-                      windowHeight = _this.$popWindow.outerHeight(),
-                      parentWidth = _this.$container.width(),
-                      parentHeight = _this.$container.height();
-
+                const curX = e.pageX,   //记录鼠标点击时的位置
+                      curY = e.pageY,
+                      windowPosition = _this.$popWindow.position(), //窗口相对容器的偏移量
+                      windowLeft = windowPosition.left,
+                      windowTop = windowPosition.top,
+                      windowMaxLeft = _this.$container.width() - _this.$popWindow.outerWidth(),
+                      windowMaxTop = _this.$container.height() - _this.$popWindow.outerHeight();
+                    
                 const active = function(e) {
-                    const curX = e.pageX,
-                          curY = e.pageY;
-                    let curLeft = winowLeft + curX - diffX,
-                        curTop = windowTop + curY - diffY;            
+                    let newX = e.pageX,       //记录鼠标移动时的位置
+                        newY = e.pageY,
+                        newLeft = windowLeft + newX - curX,      //计算得到窗口当前的偏移量
+                        newTop = windowTop + newY - curY;            
 
-                    curLeft = Math.max(0, Math.min(curLeft, parentWidth - windowWidth));
-                    curTop = Math.max(0, Math.min(curTop, parentHeight - windowHeight));
+                    newLeft = Math.max(0, Math.min(newLeft, windowMaxLeft));    //限定偏移量的范围
+                    newTop = Math.max(0, Math.min(newTop, windowMaxTop));
 
-                    _this.$popWindow.css({
-                        left: curLeft + 'px',
-                        top: curTop + 'px'
+                    _this.$popWindow.css({      //设置偏移量
+                        left: newLeft + 'px',
+                        top: newTop + 'px'
                     });
                 };
 
-                $(document).mousemove(active);
-                $(document).mouseup(function() {
+                const $document = $(document);
+                $document.mousemove(active);        //document绑定鼠标移动事件
+                $document.mouseup(function() {      //鼠标放开时解绑
                     $self.css('cursor', 'auto');
                     $(this).unbind('mousemove', active);
-                    
                 });
                 
             }
             this.$popWindow.find('.pop-window-header').mousedown(handler);
         }
 
+        destory() {
+            const _this = this;
+            this.$popWindow.find('.pop-window-close').click(function() {
+                _this.$popWindow.remove();
+            });
+        }
+
         getCurrentWindow() {
-            this.$popWindow.each(function(){
-                $(this).mousedown(function(e) {
-                    $(e.currentTarget).css('zIndex', '999').siblings().css('zIndex', '1');
-                });
+            let _this = this;
+            this.$popWindow.mousedown(function(e) {
+                $(this).appendTo(_this.$container).siblings().css();
             });
         }
 
@@ -152,7 +152,7 @@ $(document).ready(function() {
                     //鼠标悬停左边框
                     if (scopeData.left && scopeData.vertical) {
                         $this.css('cursor', 'w-resize');
-
+                        
                     //鼠标悬停右边框
                     } else if (scopeData.right && scopeData.vertical) {
                         $this.css('cursor', 'e-resize'); 
@@ -262,7 +262,6 @@ $(document).ready(function() {
 
         new PopWindow($('.container'), opt);
 
-    })
-    
-   
+    });
+ 
 });
